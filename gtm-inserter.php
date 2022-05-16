90e0e4c76723f59f974e33ef023d8dd322ddea64
Osamu Takahashi <58660677+osamunize@users.noreply.github.com>
2022-05-16 22:28:26 +0900

    値が空 or null のときにタグを出力しないようにした

--- gtm-inserter.php
+++ gtm-inserter.php
@@ -1,14 +1,16 @@
 <?php
 /**
- * @package GTM Snippet Inserter
- * @version 1.0.0
+ * Plugin Name: GTM Snippet Inserter
+ * Description: This plugin inserts GTM snippets into the site.
+ * Version: 1.0.0
+ * Author: Osamu Takahashi
+ * Author URI: https://github.com/osamunize/GTM-Snippet-Inserter
+ * License: GPL2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: gtm-snippet-inserter
+ * Domain Path: /languages
  */
-/*
-Plugin Name: GTM Snippet Inserter
-Description: サイト内に Google タグ マネージャーのスニペットを挿入するだけのシンプルなプラグインです。
-Author: Osamu Takahashi
-Version: 1.0.0
-*/
+defined( 'ABSPATH' ) || exit;
 
 /* 管理画面にオリジナルメニューを追加する */
 add_action( 'admin_menu', 'register_my_custom_menu_page' );
@@ -48,7 +50,7 @@ echo "<h2>" . __( 'GTM Snippet Inserter', 'gtm_snippet_menu' ) . "</h2>";
 ?>
 <form name="form1" method="post" action="">
 <input type="hidden" name="<?php echo $hidden_field_name; ?>" value="Y">
-<p><?php _e("GTM Snippet (GTM-XXXXXX):", 'gtm_snippet_menu' ); ?> 
+<p><?php _e("GTM Container ID (GTM-XXXXXXX):", 'gtm_snippet_menu' ); ?> 
 <input type="text" name="<?php echo $data_field_name; ?>" value="<?php echo $opt_val; ?>" size="20">
 <p class="submit">
 <input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e('Save Changes') ?>" />
@@ -59,24 +61,28 @@ echo "<h2>" . __( 'GTM Snippet Inserter', 'gtm_snippet_menu' ) . "</h2>";
 }
 
 function gtm_inserter_head(){
-$opt_val = get_option( 'gtm_snippet' );
- echo "
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','".$opt_val."');</script>
-<!-- End Google Tag Manager -->
-"."\n";}
-add_action('wp_head', 'gtm_inserter_head' , 1);
+    $opt_val = get_option( 'gtm_snippet' );
+    if ($opt_val == !null){
+        echo "
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','".$opt_val."');</script>
+        <!-- End Google Tag Manager -->
+        "."\n";}
+    }
+    add_action('wp_head', 'gtm_inserter_head' , 1);
 
 function gtm_inserter_body(){
-$opt_val = get_option( 'gtm_snippet' );
-echo '
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id='.$opt_val.'"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->	
-'."\n";}
-add_action('wp_body_open', 'gtm_inserter_body');
+    $opt_val = get_option( 'gtm_snippet' );
+    if ($opt_val == !null){
+        echo '
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id='.$opt_val.'"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->	
+        '."\n";}
+    }
+    add_action('wp_body_open', 'gtm_inserter_body');
